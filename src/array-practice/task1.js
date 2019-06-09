2eba734555aa8ad5b044eea14bafc87ecc07c2e7
rick-nice <rickman.nice@gmail.com>
2019-06-09 20:21:43 +0300

    First task of array-practice fixed. some() method used, default callback added.

--- src/array-practice/task1.js
+++ src/array-practice/task1.js
@@ -15,22 +15,8 @@
  * console.log(any([0, 0, 0, 0])); -> false
  */
 
-const any = (arr, cb) => {
-  return arr.reduce((acc, next) => {
-    if (cb !== undefined && typeof cb === 'function' && cb(next)) acc = true;
-    else if (next) acc = true;
-    return acc;
-  }, false);
+export const any = (arr, cb = a => !!a) => {
+  return arr.some(cb);
 };
-
-/*
-// Возможна реализация через some.
-export const any = (arr, cb) => {
-  if (cb !== undefined && typeof cb === 'function') {
-    return arr.some(cb);
-  }
-  return arr.some(a => !!a);
-};
-*/
 
 export default any;
