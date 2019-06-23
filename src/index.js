c39a8c51d6c46d1a55b3b4df1ce9620ac686aa57
rick-nice <rickman.nice@gmail.com>
2019-06-23 21:29:34 +0300

    added start script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './styles/main.css';
+import CHANGE from './constants/events';
+import Model from './Model';
+import View from './View';
+import Controller from './Controller';
+import { save, load } from './utilities/utilities';
 
-console.log('Hi');
+document.addEventListener('DOMContentLoaded', () => {
+  const state = load();
+  const model = new Model(state || undefined);
+
+  model.on(CHANGE, newState => save(newState));
+
+  const view = new View();
+  const controller = new Controller(model, view);
+});
