bb849886d7f68c82b7da22ce0fab75ecbfeb4332
rick-nice <rickman.nice@gmail.com>
2019-07-05 13:38:40 +0300

    Server:  adding env and config script for env

---
+++ src/server/config.js
@@ -0,0 +1,6 @@
+import 'dotenv/config';
+
+export const { PORT } = process.env;
+export const { DATABASE_URL } = process.env;
+export const { DB_USER } = process.env;
+export const { DB_PASSWORD } = process.env;
