333e52a4c480c4ba5c4414fa56eca4a797f37e2c
Yulia <jchervotkina@gmail.com>
2024-12-17 13:33:56 +0100

    Done task 1 ES modules

--- src/es-features/task1.js
+++ src/es-features/task1.js
@@ -5,19 +5,21 @@
  * console.log(task1Old(10) === 500); -> true
  */
 
-export function task1Old(a, b, c) {
-  if (b === undefined) {
-    b = 70;
-  }
+// export function task1Old(a, b, c) {
+//   if (b === undefined) {
+//     b = 70;
+//   }
 
-  if (c === undefined) {
-    c = 420;
-  }
+//   if (c === undefined) {
+//     c = 420;
+//   }
 
-  return a + b + c;
-}
+//   return a + b + c;
+// }
 
 // Напишите реализацию функции task1Old на ES6+ ниже этого комментария.
 // При желании, можете использовать стрелочную функцию, вместо обычной
 
-export function task1New() {}
+const task1New = (a, b = 70, c = 420) => a + b + c;
+
+console.log(task1New(10) === 500); // true
