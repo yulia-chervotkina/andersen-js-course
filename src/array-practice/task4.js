57108351df6970cb97e09681f61200fb9c18d423
Yulia <jchervotkina@gmail.com>
2024-12-29 19:20:40 +0100

    Done task 4 array-practice

--- src/array-practice/task4.js
+++ src/array-practice/task4.js
@@ -7,3 +7,16 @@
  * console.log(union([5, 1, 2, 3, 3], [4, 3, 2])); -> [5, 1, 2, 3, 4]
  * console.log(union([5, 1, 3, 3, 4], [1, 3, 4])); -> [5, 1, 3, 4]
  */
+
+export const union = (arr1, arr2) => {
+  const united = [...arr1, ...arr2];
+  const result = [];
+  united.forEach(el => {
+    if (!result.includes(el)) {
+      result.push(el);
+    }
+  });
+  return result;
+};
+
+export const union1 = (arr1, arr2) => [...new Set([...arr1, ...arr2])];
