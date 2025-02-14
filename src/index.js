7f2612293964ef4b64129d03b4dd6ff6225c9e7b
Yulia <jchervotkina@gmail.com>
2025-02-14 17:16:27 +0100

    added a randomiser

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import './styles/main.css';
 
-console.log('Hi');
+// A WEIGHTED RANDOMIZER FOR SELECTING AN ORE
+
+/* 
+const oresCollection = [
+  { name: 'gold', weight: 1 },
+  { name: 'silver', weight: 2 },
+  { name: 'copper', weight: 3 },
+  { name: 'brass', weight: 4 },
+  { name: 'nickel', weight: 5 },
+  { name: 'iron', weight: 6 },
+];
+
+function getRandomOre() {
+  const totalWeight = oresCollection.reduce((sum, ore) => sum + ore.weight, 0);
+  let randomNum = Math.random() * totalWeight;
+
+  let selectedOre = null;
+  oresCollection.some(ore => {
+    randomNum -= ore.weight;
+    if (randomNum <= 0) {
+      selectedOre = ore.name;
+      return true;
+    }
+    return false;
+  });
+
+  return selectedOre;
+}
+
+// const randomOre = getRandomOre();
+*/
