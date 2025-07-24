9e23cdf4294135c2b2bb70ce5d1cc8dd89a29bb0
Yulia <jchervotkina@gmail.com>
2025-07-24 09:55:33 +0200

    Adding URLs constants, improving events constants

---
+++ src/client/constants/urls.js
@@ -0,0 +1,3 @@
+export const rootURL = 'http://localhost:3000/api/recipes';
+export const favURL = 'http://localhost:3000/api/recipes/favorite';
+export const buildURL = id => `http://localhost:3000/api/recipes/${id}`;
